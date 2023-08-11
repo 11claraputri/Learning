65bdba25d881e76391dd08ad112240fd514a468f
Clariezza Deswita Putri A <11claraputri@gmail.com>
2023-08-11 16:15:33 +0800

    Update View

---
+++ components/theme/Protokol.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Protokol = () => {
+    return (
+        <div>
+            <div className="bg-black">
+                <h2 className="text-white  lg:text-xl font-medium  text-center">
+                    PROTOKOL KESEHATAN
+                </h2>
+            </div>
+        </div>
+    )
+}
+export default Protokol;
